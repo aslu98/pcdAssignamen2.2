2ab11ce9fdb994fefd977dd665d8609b3bd916d1
Asia <asia.lucchi@studio.unibo.it>
2021-04-28 16:00:47 +0200

    ok con una sola catena di operators

--- src/main/java/part3/Main.java
+++ src/main/java/part3/Main.java
@@ -1,7 +1,6 @@
 package part3;
 
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -9,25 +8,22 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Main {
 	public static void main(String[] args) throws InterruptedException {
-		LinkedList<AbstractMap.SimpleEntry<String, Integer>> mostFreq = new LinkedList<>();
 		String del = "[\\x{201D}\\x{201C}\\s'\", ?.@;:!-]+";
-		AtomicInteger analyzedWords = new AtomicInteger();
 		Input input = new Input();
+		Flag stop = new Flag();
 
 		Observable.just(input)
 				.filter(in -> in.getDir().isDirectory())
 				.flatMap(in -> getExploreObs(in.getDir()))
-				.observeOn(Schedulers.computation())
+				.observeOn(Schedulers.io())
 				.map(PDDocument::load)
 				.filter(d -> d.getCurrentAccessPermission().canExtractContent())
 				.flatMap(d -> getChunkObs(d))
 				.map(chunk -> Arrays.stream(chunk.split(del)).collect(Collectors.toList()))
-				.observeOn(Schedulers.single())
 				.scan(new HashMap<String, Integer>(), (acc, current) -> {
 					for (String word : current) {
 						Integer nf = acc.get(word);
@@ -39,17 +35,17 @@ public class Main {
 					}
 					return acc;
 				})
-				.observeOn(Schedulers.computation())
 				.filter(m -> !m.isEmpty())
 				.map(m -> m.entrySet().stream()
-						.sorted(Map.Entry.comparingByValue())
+						.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
 						.limit(input.getNumMostFreqWords())
 						.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new)))
-				.flatMap(m -> Observable.fromIterable(m.entrySet())
-										.map(entry -> entry.getKey() + " -> " + entry.getValue() + " times"))
+				.map(m -> m.toString())
 				.subscribe(Main::log);
 
-		Thread.sleep(100000);
+		while (!stop.isSet()){
+			;
+		}
 	}
 
 	private static Observable<File> getExploreObs(File inputFile){
